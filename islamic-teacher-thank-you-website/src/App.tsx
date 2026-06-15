bacfb63f300b1dd48387a9d15da1fb62bdcc67bc
sulaiman0106 <123068065+sulaiman0106@users.noreply.github.com>
2026-06-15 22:27:38 +0200

    Update App.tsx

--- islamic-teacher-thank-you-website/src/App.tsx
+++ islamic-teacher-thank-you-website/src/App.tsx
@@ -152,7 +152,7 @@ function HeroSection() {
          <img
   src="/images/logo.png"
   alt="Logo"
-  className="mx-auto h-64 w-64 object-contain mb-6"
+  className="mx-auto h-60 w-60 object-contain mb-6"
 />
         </motion.div>
 
